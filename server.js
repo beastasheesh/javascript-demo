978a9b91fe632155495effb51c3ba9b3d22fa46b
asheesh.chaudhary <asheesh.chaudhary@accenture.com>
2017-03-29 16:34:50 +0530

    DB Connection

--- server.js
+++ server.js
@@ -1,11 +1,36 @@
 var express = require('express'),
     app = express(),
-    mongoose = require('mongoose');
+    mongoose = require('mongoose'),
+    mongo = require('mongodb'),
+    assert = require('assert'),
+    Javascript = require('./models.js');
 
-mongoose.connect('mongodb://localhost:27017/javascript-demo');
+url  = 'mongodb://localhost:27017/mean-demo';
+
+addJavascript = function(req, res){
+  var javascript = new Javascript(req.body);
+  javascript.save(function(err, result){
+    res.json(result);
+  });
+}
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/api/javascript', function(req, res){
+  var resultArray = [];
+  mongo.connect(url, function(err, db){
+    assert.equal(null, err);
+    var meetups = db.collection('meetups').find();
+    meetups.forEach(function(doc, err){
+      assert.equal(null, err);
+      resultArray.push(doc);
+    }, function(){
+      db.close();
+      res.json(resultArray);
+    });
+  });
 });
 
 app.use('/', express.static(__dirname + '/'));
